e15392c44babb7a4b43ee563913ba6be3df23897
jamesjonathantossou-ayayi <jonathanelie007@gmail.com>
2026-02-18 17:38:43 +0000

    Set base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jjai-site/", // <-- IMPORTANT: must match your GitHub repo name
+  base: "/", // <-- IMPORTANT: must match your GitHub repo name
 })
